12303b9245fa5b4a65640a047b50c32f94745a05
“chandur-surge” <chandur@surge.global>
2023-02-12 08:55:34 +0530

    Initial Commit

---
+++ LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by Chandur Dissanayake on 2023-02-12.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
